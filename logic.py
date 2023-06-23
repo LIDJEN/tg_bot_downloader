2e054fd74b7c09c0aa4cf0493bd2db7399a0acf8
LIDJEN <55666193+LIDJEN@users.noreply.github.com>
2023-06-24 02:36:18 +0300

    pafy / youtube-dl

--- logic.py
+++ logic.py
@@ -1,7 +1,7 @@
+# import pytube
+# from pytube import YouTube
 import os
-import asyncio
-import pytube
-from pytube import YouTube
+import pafy
 import sqlite3
 from telebot import types
 
@@ -11,47 +11,63 @@ from telebot import types
 con = sqlite3.connect("YouTube_download_bot")
 global db
 db = con.cursor()
-db.execute("CREATE TABLE IF NOT EXISTS chat_history (chat_id int, message TEXT)")
+db.execute("CREATE TABLE IF NOT EXISTS chat_history (chat_id int, message TEXT, resolution TEXT)")
 
 
 ######################
 ## Работа с ссылкой ##
 ######################
-async def download_video(url: str, resolution: str):
-    path = "./videos"  # Path to save the video
-    video = YouTube(url)
-    stream = video.streams.get_by_resolution(resolution)
-    if stream:
-        # await asyncio.sleep(0)  # Allow other tasks to run
-        stream.download(path)
-        return video.title
-    else:
-        return None
-
-def video_resolutions(url):
-    video = pytube.YouTube(url)
-    stream=[]
-    for streams in video.streams:
-        stream.append(str(streams).split()[3])
-    return stream
 
 
-def check_if_youtube_link(url):
-    if "https://youtu.be/" in url \
-            or "https://www.youtube.com/watch?v=" in url:
-        return True
-    else:
-        return False
+def download_video(url: str, resolution: str):
+    video = pafy.new(url)
+    video.resolution = resolution
+    video.download(output_path="./videos")
+    
+def get_video_resolutions(url):
+    return ["123"]
+    video = pafy.new(url)
+    streams = []
+    for stream in video.videostreams:
+        streams.append(stream.resolution)
+    return streams
+
+# def download_video(url: str, resolution: str):
+#     path = "./videos"  # Path to save the video
+#     video = YouTube(url)
+#     stream = video.streams.get_by_resolution(resolution)
+#     if stream:
+#         # await asyncio.sleep(0)  # Allow other tasks to run
+#         stream.download(path)
+#         return video.title
+#     else:
+#         return None
+#
+# def get_video_resolutions(url):
+#     video = pytube.YouTube(url)
+#     stream=[]
+#     print(video.title)
+#     for streams in video.streams.filter(file_extension='mp4').all():
+#         stream.append(str(streams).split()[3])
+#
+#     # res = set(stream)
+#     ret = []
+#     for i in stream:
+#         # if "kbps" in i:
+#         #     continue
+#         ret.append(i[i.index("=")+2:-1])
+#     return ret
+#
+#
 
 def delete_video(name):
     os.remove(name)
 
-
 ############################################
 ## История чата запись/чтение/удаление бд ##
 ############################################
-def add_history(chat_id:int, link:str):
-    db.execute("INSERT INTO chat_history (chat_id, message) VALUES (?, ?)", (chat_id, link))
+def add_history(chat_id:int, link:str, res:str):
+    db.execute("INSERT INTO chat_history (chat_id, message, resolution) VALUES (?, ?, ?)", (chat_id, link, res))
 
 def get_history(chat_id):
     query = f"SELECT message FROM chat_history WHERE chat_id={chat_id}"
@@ -76,16 +92,9 @@ def del_history(chat_id):
 ############################
 ## Telebot reply keyboard ##
 ############################
+
 def create_inline_keyboard(options):
-    keyboard = types.InlineKeyboardMarkup()
-    for option in options:
-        label, callback_data = option
-        button = types.InlineKeyboardButton(label, callback_data=callback_data)
-        keyboard.add(button)
-    return keyboard
-
-print(video_resolutions("https://youtu.be/o17SXMKG7LU"))
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(download_video("https://youtu.be/o17SXMKG7LU", "480p"))
-
+    markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
+    for resolution in options:
+        markup.add(types.KeyboardButton(resolution))
+    return markup
